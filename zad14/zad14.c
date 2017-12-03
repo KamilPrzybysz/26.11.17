5812daf5583967cee93e1459c30c21058587bb5c
Kamil Przybysz <kprzybysz@sigma-ng.inf.ug.edu.pl>
2017-12-03 20:55:46 +0100

    zad14

---
+++ zad14/zad14.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+
+int main()
+{
+
+	int n,m;
+	printf("Podaj liczbę wierszy macierzy: \n");
+	scanf("%d",&n);
+	while(n<=0)
+	{
+		printf("Liczba wierszy macierzy musi być większa od 0: \n");
+		scanf("%d",&n);
+	}
+
+	printf("Podaj liczbę kolumn macierzy: \n");
+	scanf("%d",&m);
+	while(m<=0)
+	{
+		printf("Liczba kolumn macierzy musi być większa od 0: \n");
+		scanf("%d",&m);
+	}
+	int macierz13[n][m];
+	int macierz14[m][n];
+
+	int i, a, b, k, X, Y;
+
+	printf("Wprowadz liczby: \n");
+
+	for(i=0; i<n; i++)
+	{
+		for(a=0; a<m; a++)
+		scanf("%d",&macierz13[i][a]);
+	}
+	for(X=0;X<n;X++)
+	{
+		for(Y=0;Y<m;Y++)
+		macierz14[Y][X]=macierz13[X][Y];
+	}
+
+	printf("\n\n\nMacierz z zad 13: ");
+	for(b=0; b<n; b++)
+	{
+		printf("\n");
+		for(k=0; k<m; k++)
+		{
+			printf("%d ",macierz13[b][k]);
+		}
+	}
+
+	printf("\n\n\n Odwrócona macierz: ");
+	for(b=0; b<m; b++)
+	{
+		printf("\n");
+		for(k=0; k<n; k++)
+		printf("%d ",macierz14[b][k]);
+	}
+
+	return 0;
+}
